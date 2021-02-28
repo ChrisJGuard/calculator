cdc0933e3305e03cabba275cd5e52d51a7ba07f2
Chris Guard <christopher.guard@gmail.com>
2021-02-28 14:56:53 +0000

    Add appendToDisplay function

--- scripts/calc.js
+++ scripts/calc.js
@@ -17,3 +17,11 @@ function divide(a, b) {
 function operate(operand, a, b) {
   return operand(a, b);
 }
+
+function appendToDisplay(value) {
+  const display = document.querySelector("span");
+
+  if (display.textContent.length > 8) return;
+
+  display.textContent += value;
+}
